8e42a50b66f4776f7e6dc1b344b6f2443dfb19a1
Raphael Campagnac <rcampagnac@cegid.com>
2021-11-09 11:58:13 +0100

    Remise des scripts SQL Animaux et Espece

---
+++ Sql/dbo.Animaux.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[Animaux] (
+    [Id]          INT           IDENTITY (1, 1) NOT NULL,
+    [Nom]         NVARCHAR (50) DEFAULT ('inconnu') NOT NULL,
+    [Espece]      NVARCHAR (50) DEFAULT ('inconnu') NOT NULL,
+    [Sous-espece] NVARCHAR (50) DEFAULT ('inconnu') NOT NULL,
+    [Age]         INT           DEFAULT ((0)) NOT NULL,
+    [Sexe]        NVARCHAR (50) DEFAULT ('inconnu') NOT NULL,
+    [Statut]      NVARCHAR (50) DEFAULT ('inconnu') NOT NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC),
+    CONSTRAINT [FK_Animaux_Especes] FOREIGN KEY ([Espece]) REFERENCES [dbo].[Especes] ([EspeceAnimal])
+);
+
---
+++ Sql/dbo.Especes.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[Especes] (
+    [EspeceAnimal]        NVARCHAR (50) NOT NULL,
+    [Nombre de spécimène] INT           NULL,
+    [Classe]              NVARCHAR (50) NOT NULL,
+    [Famille]             NVARCHAR (50) NOT NULL,
+    [Sous-famille]        NVARCHAR (50) NOT NULL,
+    [régime]              NVARCHAR (50) NOT NULL,
+    CONSTRAINT [PK_Especes] PRIMARY KEY CLUSTERED ([EspeceAnimal] ASC)
+);
+
