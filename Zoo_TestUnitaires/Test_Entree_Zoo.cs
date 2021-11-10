e693cf43d16054fb776895f02c41a69a92155f85
Raphael Campagnac <rcampagnac@cegid.com>
2021-11-10 11:00:04 +0100

    Ajout doc rapport

---
+++ Zoo_TestUnitaires/Test_Entree_Zoo.cs
@@ -0,0 +1,24 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Zoo_Raph.ZOO_RaphDataSetTableAdapters;
+using Microsoft.VisualStudio.TestPlatform;
+
+namespace Zoo_TestUnitaires
+{
+    [TestClass]
+    public class Test_Entree_Zoo
+    {
+        [TestMethod]
+        public void Test_Btn_Taureau()
+        {
+            //Arrange
+
+            //Act
+
+
+            //Assert
+
+
+
+        }
+    }
+}
